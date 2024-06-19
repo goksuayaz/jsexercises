76284ada1c56d545a94ac8b67fe71168b0ceeabc
Göksu Ayaz <goksuayaz79@gmail.com>
2024-06-19 16:30:31 +0300

    jscalismasi

---
+++ app.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+//console.log("Goksu");
+
+
+let a=11;
+let b=21;
+
+alert("İki sayının toplamı :" + (a+b));
+
+
+console.log("window");
+console.log(location.port);
+
+console.error("Hata oluştu");
+console.warn("Hata");
+
+// console.log(location.port);
+// debugger;
+// console.log(location.port);
+// console.log("Var");
+
+/*
+
+! Dikkat dikkat
+? Öyle mi
+TODO: Ya ben
+*Burası çok önemli
+
+*/
