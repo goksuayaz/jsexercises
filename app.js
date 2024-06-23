f45489de18602c8268999f4c4242ff16eacc4948
Göksu Ayaz <goksuayaz79@gmail.com>
2024-06-23 17:21:08 +0300

    loops

--- app.js
+++ app.js
@@ -78,11 +78,15 @@ TODO: Ya ben
 
 
 
+
 // let ad=prompt("İsminizi giriniz :");
 // let tckn = prompt("TCKN giriniz :");
 
-// //kontrolEt(ad, tckn);
+
 // kontrolEt2(ad, tckn);
+
+
+
 
 
 // function kontrolEt(ad, tckn){
@@ -214,11 +218,11 @@ TODO: Ya ben
 //     break;
 
 //     case "2":
-//         console.log("Giriklen sayı 2'dir");
+//         console.log("Girilen sayı 2'dir");
 //         break;
 
 //    case "3":
-//     console.log("Giriken sayı 3'tür");
+//     console.log("Girilen sayı 3'tür");
 //     break; 
     
 //     case "4":
@@ -234,3 +238,252 @@ TODO: Ya ben
 //         break;
 
 // }
+
+
+// ATM Uygulaması
+
+
+// let bakiye=1000;
+// let yeniSatir="\r\n";
+
+
+// let metin="1-Bakiye Görüntüleme "+ yeniSatir
+// +"2-Para Çekme " + yeniSatir
+// + "3-Para Yatırma" + yeniSatir
+// +"4-Çıkış" + yeniSatir
+// +"Lütfen bir değer seçiniz.";
+
+
+// let secim=prompt(metin);
+// switch(secim){
+//     case "1":
+//         alert("Bakiyeniz : " + bakiye);
+//         break;
+
+//     case "2":
+//         let cekilecekTutar= Number(prompt("Çekmek istediğiniz tutarı giriniz :"));
+//         if(cekilecekTutar < bakiye){
+//             bakiye = bakiye-cekilecekTutar;
+//             alert("Kalan bakiye :" + bakiye);
+//         }else{
+//             alert("Bakiyenizden fazla para çekemezsiniz!" +yeniSatir
+//                 + "Bakiyeniz : " + bakiye + " " + "Cekilecek Tutar : " + cekilecekTutar);
+
+//         }
+//         break;
+
+
+//      case "3":
+//         let yatirilacakTutar = Number(prompt("Yatırmak istediğiniz tutarı giriniz :"));
+        
+//         bakiye = bakiye+yatirilacakTutar;
+
+//         alert("Güncel bakiyeniz :" + bakiye);
+//         break;
+
+
+//      case "4":
+//         alert("İyi günler");
+//         break;   
+
+//     default:
+//         alert("Lütfen 1-4 arasında bir seçim yapınız.");
+//         break;
+
+
+// }
+
+//TYT PUAN HESAPLAMA
+
+// let turkceDogru, turkceYanlis = 0;
+
+// let matematikDogru, matematikYanlis=0;
+
+// let sosyalDogru, sosyalYanlis = 0;
+
+// let fenDogru, fenYanlis=0;
+
+// let puan=0;
+// let okulPuani=0;
+
+
+
+// let yeniSatir = "\r\n";
+
+// let mesaj = "TYT Puan Hesaplama uygulamasına hoşgeldiniz!"+ yeniSatir
+// + "1-Puan hesapla" + yeniSatir
+// +"2-Çıkış yap";
+
+//alert(mesaj);
+
+// let secim = prompt(mesaj);
+
+// switch(secim){
+//     case "1":
+
+   
+//         turkceDogru = Number(prompt("Türkçe Doğru Sayısı :"));
+//         turkceYanlis = Number(prompt("Türkçe Yanlış Sayısı: "));
+        
+
+//         matematikDogru = Number(prompt("Mat Doğru Sayısı :"));
+//         matematikYanlis = Number(prompt("Mat Yanlış Sayısı: "));
+
+
+//         sosyalDogru = Number(prompt("Sosyal Doğru Sayısı :"));
+//         sosyalYanlis = Number(prompt("Sosyal Yanlış Sayısı: "));
+
+//         fenDogru = Number(prompt("Fen Doğru Sayısı :"));
+//         fenYanlis = Number(prompt("Fen Yanlış Sayısı: "));
+
+//         okulPuani = Number(prompt("Okul Puanı :"));
+
+
+//        let dogruSayisi = turkceDogru+matematikDogru+fenDogru+sosyalDogru;
+//        let yanlisSayisi = turkceYanlis+matematikYanlis+fenYanlis+sosyalYanlis;
+
+//        let net = dogruSayisi - (yanlisSayisi/4);
+
+//        puan = (net*4) + 100 + okulPuani;
+
+//        alert("TYT Puanınız : " + puan);
+
+//        break;
+
+//      case "2":
+//         alert("Sistemden çıkış yapıldı...");
+//         break;
+        
+//      default:
+//         alert("Lütfen geçerli aralıkta değer giriniz! ");
+//         break;   
+
+// }
+
+
+//TÜR DÖNÜŞÜMLERİ
+
+
+// let a = 5;
+
+// let b = "10";
+
+// let c = Number(b);
+
+// console.log(typeof c);
+
+// console.log(a+c);
+
+//veya let b= Number("10");
+
+
+//parseInt()
+
+//console.log(window);
+
+// let a = 8;
+// let b = "12";
+
+//let b = parseInt("12"); olarak yazılabilir.
+// console.log(a+b);
+
+// parseInt noktadan sonrasını atıyor, parseFloat noktadan sonrasını da alıyor.
+
+
+//Number tipinden string veri tipine dönüştürme
+
+// let x = 55;
+
+// let x = String(55);
+
+// console.log(typeof x);
+
+
+// veya let x = (55).toString();
+
+
+// Boolean dan String e dönüştürme
+
+// let sonuc = true;
+// let sonuc = String(true);
+
+
+// Object i String e dönüştürme
+
+// let rakamlar = [1,2,3,4]; 
+// let rakamlar = String([1,2,3,4]);
+
+// console.log(typeof rakamlar);
+
+
+// let rakamlar = Number([1,2,3,4]);  
+
+// console.log(rakamlar); -> NaN(not a number)
+
+
+
+
+// let a = 5;
+// debugger;  -> sources kısmında tek tek kodları incelemeni sağlar.
+
+// let b= 15;
+
+
+
+// for(let i=1; i<=10; i++){
+//     if(i%2==1){
+
+//         console.log("Goksu");
+//     }else{
+//         console.lof("Ayaz");
+
+
+//     }
+// }
+
+
+
+// let toplam = 0;
+
+// for(i=50; i>=1;i--){
+//     toplam +=i;
+//     console.log(i);
+// }
+
+// console.log("Toplam : ", toplam);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
