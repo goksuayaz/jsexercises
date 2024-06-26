91b9075e0f3466aca5db6f439b92fb2014dd36ce
Göksu Ayaz <goksuayaz79@gmail.com>
2024-06-26 15:47:38 +0300

    arrays

--- app.js
+++ app.js
@@ -794,6 +794,346 @@ TODO: Ya ben
 
 
 
+// let sayilar=[0,1,2,3,4,5,6,7,8,9];
+
+// console.log(sayilar[6]);
+// sayilar[8]="Goksu";
+
+// console.log(sayilar[8]);
+
+
+// let isimler=["Goksu","Ali","Ayşe"];
+
+// console.log(isimler[2]);
+// isimler[1] = "Yusuf";
+
+
+// let karisikDizi =[1,"E", 5.7, true, null, undefined];
+// console.log(karisikDizi[3]);
+
+
+
+
+
+// let dizi1= new Array();
+// let dizi2=[];
+
+// dizi2[0]="E";
+// dizi2[1]="A";
+
+
+// dizi1=["E", "A"];
+
+
+
+
+
+
+// let isimler=["Enes", "Yakup", "Bilal", "Ayşe"];
+
+
+// for(let i=0; i<isimler.length; i++){
+//     console.log(isimler[i]);
+
+// }
+
+// isimler.forEach(function(isim){
+
+//     console.log(isim);
+// });
+
+
+
+
+
+
+
+// let arabalar1=["Toyota", "Mercedes", "Porsche"];
+// let arabalar2=["Hyundai", "Renault"];
+
+
+//Push:Dizinin sonuna eleman ekler.
+// console.log(arabalar.length);
+
+// let diziUzunluk=arabalar.push("opel");
+
+// console.log(diziUzunluk);
+
+
+
+//Unshift: dizinin başına eleman ekler.
+// arabalar.unshift("Hyundai");
+
+// console.log(arabalar);
+
+
+
+
+//Pop: son elemanı siler ve silineni geriye döner.
+// let silinenEleman=arabalar.pop();
+// console.log(arabalar);
+// console.log(silinenEleman);
+
+
+
+//Shift: dizinin başından eleman siler.
+
+// let silinenEleman=arabalar.shift();
+// console.log(silinenEleman);
+
+
+
+
+//Splice: eleman eklemek ve silmek için kullanılır.
+
+// console.log(arabalar);
+
+// arabalar.splice(2,0,"hyundai");
+// console.log(arabalar);
+
+
+
+
+//Tostring metot
+
+// let stringArabalar=rabalar.toString();
+// console.log(typeof stringArabalar);
+// console.log(stringArabalar);
+
+
+
+//Join metot: diziyi stringe çevirir. Farkı ise araya eleman ekleyebiliriz.
+
+// let stringArabalar=arabalar.join("-");
+// console.log(stringArabalar);
+
+
+
+
+
+//Concat: dizileri birleştirmek için kullanılır.
+
+// let birlesmisDizi=arabalar1.concat(arabalar2);
+
+// console.log(birlesmisDizi);
+
+
+
+//Slice: diziyi istediğin yerden böler, ayrı bir dizi oluşturur.
+
+// console.log(arabalar1);
+
+// let ayriDizi=arabalar1.slice(1);
+// console.log(ayriDizi);
+
+
+
+//Length: özellik
+// console.log(arabalar.length);
+
+
+
+//Reverse: ters çevirir
+// console.log(arabalar);
+// arabalar.reverse();
+// console.log(arabalar);
+
+
+//split: belirli bir ifadeye göre bölüp diziye çevirmek.
+
+// let isimler="E, A, V";
+
+// let isimlerDizi=isimler.split(" , ");
+// console.log(isimlerDizi);
+
+
+
+
+//indexof : içine bir değer veriyorsan, eğer ki o elemanı içeriyorsa 0 yoksa -1 dönüyor.
+
+// let index=arabalar1.indexOf("bmw");
+// console.log(index);
+
+
+
+
+//includes: eleman var mı yok mu ona bakıyor. True veya false döndürür.
+
+// let sonuc=arabalar1.includes("porsche");
+// console.log(sonuc);
+
+
+
+
+
+
+
+
+
+
+
+// let urun1={
+//     isim: "ACER Swift",
+//     kategori: "Teknoloji",
+//     fiyat : 6.219
+
+// }
+
+// let urun2={
+//     isim: "ACER Sw",
+//     kategori:"Teknoloji",
+//     fiyat : 7.120
+
+// }
+
+// let urun3={
+//     isim:"ACER S",
+//     kategori: "Teknoloji",
+//     fiyat : 5.450
+
+// }
+
+// let urun4={
+//     isim:"Lenovo V15",
+//     kategori:"Teknoloji",
+//     fiyat : 8.350
+// }
+
+// let urun5={
+//     isim:"Lenovo V14",
+//     kategori:"Teknoloji",
+//     fiyat : 6.750
+// }
+
+// let urunler = [urun1, urun2, urun3, urun4, urun5];
+// let filtreliUrunler=[];
+// let kullaniciUrunIsmi=prompt("Bir ürün ismi giriniz :");
+
+// filtreliUrunleriDoldur(urunler);
+// filtreliUrunleriYazdir(filtreliUrunler);
+
+
+
+// function filtreliUrunleriDoldur(urunler){
+//     urunler.forEach(function(urun){
+//         if(urun.isim.toUpperCase().includes(kullaniciUrunIsmi.toUpperCase(), 0)){
+//             filtreliUrunler.push(urun);
+    
+//         }
+    
+//     });
+
+// }
+
+
+// function filtreliUrunleriYazdir(urunler){
+
+//     urunler.forEach(function(urun){
+//         console.log("----------------------------------");
+//         console.log("|" + urun.isim + "|" + urun.fiyat + "|" + urun.kategori);
+//         console.log("----------------------------------");
+
+//     });
+// }
+
+
+
+
+
+
+
+
+
+// let kitap1={isim : "Her Şeyi Düşünme", yazar : "Anne Bogel", fiyat : 25, raf:"1.5.RAF"}
+// let kitap2={isim : "Hiçbir Karşılaşma Tesadüf Değildir", fiyat : 56, raf:"2.3.RAF"}
+// let kitap3={isim : "İnsan Neyle Yaşar", yazar : "Tolstoy", fiyat : 34, raf:"3.4.RAF"}
+// let kitap4={isim : "Zafer Sızlanarak Kazanılmaz", yazar:"Haluk Tatar", fiyat : 45, raf:"4.1.RAF"}
+// let kitap5={isim : "Şeker Portakalı", yazar : "Jose Mauro", fiyat : 25, raf:"5.3.RAF"}
+
+// let kitaplar=[kitap1, kitap2, kitap3, kitap4, kitap5];
+
+
+
+// let raf11={kod : "1.1 RAF", goster:false}
+// let raf12={kod : "1.2 RAF" , goster:false}
+// let raf13={kod : "1.3 RAF" , goster:false}
+// let raf14={kod : "1.4 RAF" , goster:false}
+// let raf15={kod : "1.5 RAF" , goster:false}
+
+
+// let raf21={kod : "2.1 RAF", goster:false}
+// let raf22={kod : "2.2 RAF" , goster:false}
+// let raf23={kod : "2.3 RAF" , goster:false}
+// let raf24={kod : "2.4 RAF" , goster:false}
+// let raf25={kod : "2.5 RAF" , goster:false}
+
+
+
+
+// let raf31={kod : "3.1 RAF", goster:false}
+// let raf32={kod : "3.2 RAF" , goster:false}
+// let raf33={kod : "3.3 RAF" , goster:false}
+// let raf34={kod : "3.4 RAF" , goster:false}
+// let raf35={kod : "3.5 RAF" , goster:false}
+
+
+
+
+// let raf41={kod : "4.1 RAF", goster:false}
+// let raf42={kod : "4.2 RAF" , goster:false}
+// let raf43={kod : "4.3 RAF" , goster:false}
+// let raf44={kod : "4.4 RAF" , goster:false}
+// let raf45={kod : "4.5 RAF" , goster:false}
+
+
+
+// let raf51={kod : "5.1 RAF", goster:false}
+// let raf52={kod : "5.2 RAF" , goster:false}
+// let raf53={kod : "5.3 RAF" , goster:false}
+// let raf54={kod : "5.4 RAF" , goster:false}
+// let raf55={kod : "5.5 RAF" , goster:false}
+
+
+
+
+// let raflar=[
+//     [raf51, raf52, raf53, raf54, raf55],
+//     [raf41, raf42, raf43, raf44, raf45],
+//     [raf31, raf32, raf33, raf34, raf35],
+//     [raf21, raf22, raf23, raf24, raf25],
+//     [raf11, raf12, raf13, raf14, raf15]
+
+// ];
+
+
+
+// function rafOlustur(){
+//     console.clear();
+//     let satir="";
+
+//     for(let i=0; i<raflar.length; i++ ){
+//         for(j=0; j<5; j++){
+    
+//             satir+="|" + (raflar[i][j].goster ? raflar[i][j].kod : "-----") + "|";
+            
+//         }
+        
+//         console.log(satir);
+//         satir="";
+
+//     }
+    
+// }
+
+// rafOlustur();
+
+
+
+
+
+
+
+
 
 
 
